7cb2ab1e4d516910678754d6f07ec57b011a2659
Patrick <patrickgobox@gmail.com>
2019-06-10 22:42:28 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 function updateFollower() {
-    fetch('http://plus.zimpatrick.gq/zimnew/follower')
+    fetch('https://plus.zimpatrick.gq/zimnew/follower')
     .then(resp=>resp.text())
     .then(text=>document.getElementById('follower_count').innerHTML=text);
 }
 function updateSubcount() {
-    fetch('http://plus.zimpatrick.gq/zimnew/abos')
+    fetch('https://plus.zimpatrick.gq/zimnew/abos')
     .then(resp=>resp.text())
     .then(text=>document.getElementById('sub_count').innerHTML=text);
 }

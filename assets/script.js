f2628eae699773cb5881c4371ddc651ca67ba269
zImPatrick <patrickgobox@gmail.com>
2019-08-23 01:15:42 +0200

    stuff

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,1 @@
-function updateFollower() {
-    fetch('https://plus.zimpatrick.gq/zimnew/follower')
-    .then(resp=>resp.text())
-    .then(text=>document.getElementById('follower_count').innerHTML=text);
-}
-function updateSubcount() {
-    fetch('https://plus.zimpatrick.gq/zimnew/abos')
-    .then(resp=>resp.text())
-    .then(text=>document.getElementById('sub_count').innerHTML=text);
-}
-
-updateFollower();
-updateSubcount();
+// ich hab hier nichts?

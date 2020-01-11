9b6bf661569e7353a939e22dc6e24d7f9e40aff5
Patrick <patrickgobox@gmail.com>
2020-01-12 00:21:50 +0100

    yes

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,33 @@
-// ich hab hier nichts?
+var get = q => document.querySelector(q); 
+
+// schlechter code von mir
+var c = "";
+var co = [
+  "arrowup",
+  "arrowup",
+  "arrowdown",
+  "arrowdown",
+  "arrowleft",
+  "arrowright",
+  "arrowleft",
+  "arrowright",
+  "b",
+  "a"
+];
+addEventListener("keydown", e => {
+  if (e.key.toLowerCase() == co[c.length]) {
+    c = c + 1;
+    if (c.length == 10) {
+        console.log("should do something");
+        var stuff = get("#stuff"), main = get("#main");
+      stuff.style.webkitAnimationPlayState = "running";
+      main.style.webkitAnimationPlayState = "running";
+      stuff.style.animationPlayState = "running";
+      main.style.animationPlayState = "running";
+      stuff.style.animationName = "secret";
+      main.style.animationName = "secret";
+      get("#secret").style.transform = "translateX(0px) translateY(-200px)"
+      get("#secret").style.display = "block";
+    }
+  } else c = "";
+});

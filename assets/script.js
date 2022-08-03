66be085a64451b24cc52df3a3c7bd14e94734705
Patrick <23613354+zImPatrick@users.noreply.github.com>
2022-08-03 14:37:36 +0200

    Discord auf dem System detecten und verwenden

--- assets/script.js
+++ assets/script.js
@@ -51,3 +51,15 @@ if (navigator.userAgent.includes("Firefox")) {
     "font-size:18px;"
   );
 }
+
+// Discord detecten und mit URL-Protocol launchen
+// (ich wünschte wir könnten in JS einfach URL-Protocols detecten)
+document.querySelector('.icon.discord').addEventListener('click', e => {
+  let timeout = setTimeout(() => {
+    window.location = e.target.href;
+  }, 250)
+  window.addEventListener('blur', () => clearTimeout(timeout), { once: true });
+
+  window.location = 'discord://-/users/137228543045140480';
+  e.preventDefault();
+});
